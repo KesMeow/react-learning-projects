dee2f1bf0b72b8731d2400ddc89a7c609d121332
Yikun Zhou <kesterzhou@outlook.com>
2022-02-11 15:21:06 -0800

    ReactFacts Project Finish

--- react-course-project-1/index.js
+++ react-course-project-1/index.js
@@ -1,16 +1,5 @@
-/**
-Challenge: Project setup
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
 
-- Create an App component outside the components folder (sibling to
-  the index.js file)
-    - Have App render the Navbar and Main components
-- Import and render the App component inside of index.js using ReactDOM
-    - At this point you should have your "Navbar goes here" etc. showing up
-      in the mini-browser.
-- Go to Google fonts and get the "Inter" font with weights 400, 600, and 700.
-  Put the links to those fonts ABOVE the style.css link in index.html (Use
-  the `<link/>` elements instead of the @import or npm options for getting
-  the fonts. You may need to do some extra research to figure out how this 
-  works if you haven't done it before)
-*/
-
+ReactDOM.render(<App />, document.getElementById("root"))
